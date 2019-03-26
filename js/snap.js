9378779abadf1bc389d94dec581c465aeb249c05
Daniel Miller <lontano.composer@gmail.com>
2019-03-26 11:49:27 -0400

    push all

---
+++ js/snap.js
@@ -0,0 +1,14 @@
+$.scrollify({
+    section: ".section",
+    sectionName: "section-name",
+    interstitialSection: "",
+    easing: "easeOutCubic",
+    scrollSpeed: 2000,
+    offset: 0,
+    scrollbars: true,
+    standardScrollElements: "",
+    setHeights: true,
+    overflowScroll: true,
+    updateHash: true,
+    touchScroll: true,
+});
